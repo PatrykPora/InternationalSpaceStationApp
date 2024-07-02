4f7ce6e87b06e9c8c45639f4043d24e7deb7a957
PatrykPora <patryk.pora@shitcoins.club>
2024-07-02 18:21:19 +0200

    logger added, base hibernate congig added

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,7 +9,11 @@ module pl.patrykpora.internationalspacestation {
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
+    requires eu.hansolo.fx.countries;
+    requires eu.hansolo.toolbox;
+    requires eu.hansolo.toolboxfx;
     requires com.almasb.fxgl.all;
+    requires org.slf4j;
 
     opens pl.patrykpora.internationalspacestation to javafx.fxml;
     exports pl.patrykpora.internationalspacestation;
--- src/main/java/pl/patrykpora/internationalspacestation/InternationalSpaceStationApplication.java
+++ src/main/java/pl/patrykpora/internationalspacestation/InternationalSpaceStationApplication.java
@@ -4,13 +4,19 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class InternationalSpaceStationApplication extends Application {
+
+    public static final Logger logger = LoggerFactory.getLogger(InternationalSpaceStationApplication.class);
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        logger.info("starting application......");
+        FXMLLoader fxmlLoader = new FXMLLoader(InternationalSpaceStationApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
